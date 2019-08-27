ab358b8044c5044f02d01cfc4e3c0f1be00cc98c
黄威龙 <962589950@qq.com>
2019-08-27 09:32:23 +0800

    新增事务处理类

---
+++ BLL/Affair.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace BLL
+{
+    //事务类
+    public partial class Affair
+    {
+        public static T Whether<T>(T t,Func<T,T> action)
+        {
+
+            TransactionOptions transactionOptions = new TransactionOptions();
+            //设置事务隔离级别
+            transactionOptions.IsolationLevel = System.Transactions.IsolationLevel.ReadCommitted;
+            // 设置事务超时时间为120秒
+            transactionOptions.Timeout = new TimeSpan(0, 0, 120);
+
+            using (TransactionScope scope = new TransactionScope(TransactionScopeOption.Required, transactionOptions))
+            {
+
+                try
+                {
+                    t= action.Invoke(t);
+                    scope.Complete();
+                    return t;
+                   
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                 
+                }
+            }
+
+        }
+    }
+}
